e0a74a4bc4a927c48d580b93e9d765d45f7547da
Ameen <35753638+techlancing@users.noreply.github.com>
2021-12-21 10:33:52 +0530

    application name changed and passbook print pushed down

--- admin/src/environments/environment.ts
+++ admin/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  applicationName: "suryafarmerssociety",
+  applicationName: "suryacooperativesocietylimited",
   apiUrl: "/",
   imagePath: "http://localhost:2155/",
   defaultauth: 'fackbackend',
